149e09c5f2aaee68f601c20b9a204f69222741c1
Mike Buzzetti <mike.buzzetti@gmail.com>
2014-11-23 15:56:03 -0500

    Worknig version

---
+++ rpmsign/rpmsign.py
@@ -0,0 +1,35 @@
+import xml.etree.ElementTree as XML
+
+def rpmsign(parser, xml_parent, data):
+    """yaml: rpmsign
+    This plugin adds a post-build step to sign rpms using GPG.
+
+    Requires the Jenkins `RPM Sign Plugin.
+    <https://wiki.jenkins-ci.org/display/JENKINS/RPM+Sign+Plugin>`_
+    :arg list rpms:
+        :rpms:
+            * **gpgKeyName** (`str`) - Name of the gpg key
+            * **includes** (`str`) - Path to the rpm(s) sign
+            * **cmdlineOpts** (`str`) - Optional command line options to rpmsign
+            * **resign** (`bool`) -Resign the rpm 
+                (default false)
+
+    Example:
+    .. literalinclude:: /../../sample/job.yaml
+    """
+    rpm_sign = XML.SubElement(xml_parent, 
+        "jenkins.plugins.rpmsign.RpmSignPlugin"
+    )
+        
+    entries = XML.SubElement(rpm_sign, 'entries')
+        
+    for entry in data['rpms']:
+        rpm = XML.SubElement(entries, 'jenkins.plugins.rpmsign.RPM')
+        XML.SubElement(rpm, 'gpgKeyName').text = entry['gpgKeyName']        
+        XML.SubElement(rpm, 'includes').text = entry['includes']
+        XML.SubElement(rpm, 'cmdlineOpts').text = entry.get('cmdlineOpts', '')
+        
+        if entry.get('resign', False):
+            XML.SubElement(rpm, 'resign').text = 'false'
+        else:
+            XML.SubElement(rpm, 'resign').text =  'true'
---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup,find_packages
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "jenkins-job-builder-rpmsign",
+    version = "0.0.3",
+    author = "Mike Buzzetti",
+    author_email = "mike.buzzetti@gmail.com",
+    description = ("Jenkins-job-builder that adds support for Jenkins RPMSign plugin"),
+    license = "OSI",
+    keywords = "jenkins, plugin",
+    url = "http://github.com/jimbydamonk/jenkins-job-builder-rpmsign",
+    packages=find_packages(),
+    long_description=read('README.md'),
+    install_requires=["jenkins-job-builder>=0.6.0"],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python'
+    ],
+    entry_points={
+        'jenkins_jobs.publishers': [
+            'rpmsign=rpmsign:rpmsign',
+        ],
+    }
+)
